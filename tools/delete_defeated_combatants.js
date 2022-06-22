8e3554ed4c90940d1d77b171c7e91c01b878b074
Zhell <50169243+krbz999@users.noreply.github.com>
2022-06-22 15:19:08 +0200

    Add files via upload

---
+++ tools/delete_defeated_combatants.js
@@ -0,0 +1,16 @@
+// delete combatants from the combat tracker if and only if they have 0 or less hp and do not have a player owner.
+// required modules: none.
+
+// the ids of the combatants to delete.
+const ids = game.combat.combatants.filter(i => {
+	// filter out player-owned combatants.
+	if(i.hasPlayerOwner) return false;
+	
+	// filter out living combatants
+	if(getProperty(i.token, "actor.data.data.attributes.hp.value") > 0) return false;
+	
+	return true;
+}).map(i => i.id);
+
+// yeet 'em.
+await game.game.combat.deleteEmbeddedDocuments("Combatant", ids);
