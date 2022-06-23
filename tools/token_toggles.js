334805b66fc89062908ede493423763f911ede9d
Zhell <50169243+krbz999@users.noreply.github.com>
2022-06-23 13:05:46 +0200

    Add files via upload

---
+++ tools/token_toggles.js
@@ -0,0 +1,19 @@
+// swap selected token's img and size.
+
+// toggles.
+const swapSize = true;
+const swapImg = true;
+
+// array of changes.
+const imgs = ["imageA.webp", "imageB.webp"];
+const sizes = [1, 2];
+
+// the update object.
+const updates = {}
+if(swapSize) updates.img = token.document.data.img === imgs[0] ? imgs[1] : imgs[0];
+if(swapImg) updates = mergeObject(updates, {
+	height: token.document.data.height === sizes[0] ? sizes[1] : sizes[0],
+	width: token.document.data.height === sizes[0] ? sizes[1] : sizes[0]
+});
+
+await token.document.update(updates);
