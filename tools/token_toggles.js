d71da8d8f7683b2fe04321c163192e0e42bc7209
Zhell <50169243+krbz999@users.noreply.github.com>
2022-06-27 14:47:54 +0200

    Update token_toggles.js

--- tools/token_toggles.js
+++ tools/token_toggles.js
@@ -1,4 +1,5 @@
 // swap selected token's img and size.
+// add image paths and size changes below:
 
 // toggles.
 const swapSize = true;
@@ -12,8 +13,8 @@ const sizes = [1, 2];
 const updates = {}
 if(swapSize) updates.img = token.document.data.img === imgs[0] ? imgs[1] : imgs[0];
 if(swapImg) updates = mergeObject(updates, {
-	height: token.document.data.height === sizes[0] ? sizes[1] : sizes[0],
-	width: token.document.data.height === sizes[0] ? sizes[1] : sizes[0]
+  height: token.document.data.height === sizes[0] ? sizes[1] : sizes[0],
+  width: token.document.data.height === sizes[0] ? sizes[1] : sizes[0]
 });
 
 await token.document.update(updates);
