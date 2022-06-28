00549c837f8cb8c4ebe9771e83c0161eb98ce930
Zhell <50169243+krbz999@users.noreply.github.com>
2022-06-28 02:40:06 +0200

    Update set_token_dispositions.js

--- tools/set_token_dispositions.js
+++ tools/set_token_dispositions.js
@@ -1,25 +1,40 @@
-// set all selected tokens as friendly, neutral, hostile
+// single dialog to configure all selected tokens' disposition, displayBars, and displayName configuration.
 
-const options = Object.entries(CONST.TOKEN_DISPOSITIONS).reduce((acc, [key,value]) => acc += `<option value="${value}">${key}</option>`, ``);
+const options_disposition = Object.entries(CONST.TOKEN_DISPOSITIONS).reduce((acc, [key,value]) => acc += `<option value="${value}">${key}</option>`, ``);
+const options_display = Object.entries(CONST.TOKEN_DISPLAY_MODES).reduce((acc, [key, value]) => acc += `<option value="${value}">${key}</option>`, ``);
 
 new Dialog({
-  title: "Token Disposition",
+  title: "Token Configuration",
   content: `
   <form>
     <div class="form-group">
-      <label for="disposition">Disposition</label>
-      <div class="form-fields">
-        <select id="disposition">${options}</select>
-      </div>
+    <label for="disposition">Disposition</label>
+    <div class="form-fields">
+      <select id="disposition">${options_disposition}</select>
     </div>
+  </div>
+  <div class="form-group">
+    <label for="display-bars">Display Bars</label>
+    <div class="form-fields">
+      <select id="display-bars">${options_display}</select>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="display-name">Display Name</label>
+    <div class="form-fields">
+      <select id="display-name">${options_display}</select>
+    </div>
+  </div>
   </form>`,
   buttons: {
     go: {
-      icon: "<i class='fas fa-check'></i>",
+      icon: `<i class="fas fa-check"></i>`,
       label: "Apply",
       callback: async (html) => {
         const disposition = Number(html[0].querySelector("select[id=disposition]").value);
-        const updates = canvas.tokens.controlled.map(i => ({_id: i.id, disposition}));
+        const displayBars = Number(html[0].querySelector("select[id='display-bars']").value);
+        const displayName = Number(html[0].querySelector("select[id='display-name']").value);
+        const updates = canvas.tokens.controlled.map(i => ({_id: i.id, disposition, "bar1.attribute": "attributes.hp", displayBars, displayName}));
         await canvas.scene.updateEmbeddedDocuments("Token", updates);
       }
     }
