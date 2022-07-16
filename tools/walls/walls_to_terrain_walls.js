d09c4f3f69d18bafb5dbee0ba8380d39afc789f7
Zhell <50169243+krbz999@users.noreply.github.com>
2022-07-16 12:59:37 +0200

    Update walls_to_terrain_walls.js

--- tools/walls/walls_to_terrain_walls.js
+++ tools/walls/walls_to_terrain_walls.js
@@ -4,7 +4,7 @@ const {NORMAL, LIMITED} = CONST.WALL_SENSE_TYPES;
 const updates = canvas.scene.walls.filter(i => {
   // only walls that are set up exactly as regular walls (blocking everything).
   const {light, move, sight, sound} = i.data;
-  return light === NORMAL && move === NORMAL && sight === NORMAL && sound === NORMAL;
+  return new Set([light, move, sight, sound, NORMAL]).size === 1;
 }).map(i => {
   // set blocking of light, sight, and sound to terrain defaults.
   return {_id: i.id, light: LIMITED, sight: LIMITED, sound: LIMITED};
