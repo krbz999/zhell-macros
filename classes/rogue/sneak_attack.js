536f303f0103ad003928e667b7b8a49bf6f3a8d4
Zhell <50169243+krbz999@users.noreply.github.com>
2022-07-17 13:11:36 +0200

    Update sneak_attack.js

--- classes/rogue/sneak_attack.js
+++ classes/rogue/sneak_attack.js
@@ -1,16 +1,22 @@
 // Sneak Attack
 // Required modules: itemacro
 
+// applies an effect that increases weapon damage equal to sneak attack.
+// click again to remove the effect.
+
 // if effect, delete it
 const effect = actor.effects.find(i => i.data.label === "Sneak Attack");
 if(effect) return effect.delete();
 
 // if no effect, create it
 const mode = CONST.ACTIVE_EFFECT_MODES.ADD;
-const value = `+${item.getChatData().damage.parts[0][0]}`;
+const value = `+${item.data.data.damage.parts[0][0]}`;
 
 await actor.createEmbeddedDocuments("ActiveEffect", [{
-	changes: [{key: "data.bonuses.mwak.damage", mode, value}, {key: "data.bonuses.rwak.damage", mode, value}],
+	changes: [
+		{key: "data.bonuses.mwak.damage", mode, value},
+		{key: "data.bonuses.rwak.damage", mode, value}
+	],
 	icon: item.img,
 	label: "Sneak Attack",
 	"flags.core.statusId": "sneak-attack"
