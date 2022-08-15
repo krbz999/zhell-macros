49cd7e8b1ada4a123fed45f4804974d43116b81a
Zhell <50169243+krbz999@users.noreply.github.com>
2022-08-15 21:41:24 +0200

    Update harness_divine_power.js

--- classes/cleric/harness_divine_power.js
+++ classes/cleric/harness_divine_power.js
@@ -1,64 +1,65 @@
 /* Harness Divine Power
  * Regain one expended spell slot, the level of which can
  * be no higher than half your proficiency bonus (rounded up).
- * Requires Item Macro.
+ * Required modules: itemacro.
  */
 
-const {prof} = actor.getRollData().attributes;
-const spells = duplicate(actor.getRollData().spells);
+const rollData = foundry.utils.duplicate(actor.getRollData());
+const {spells, attributes: {prof}} = rollData;
+const maxLevel = Math.ceil(prof/2);
+const validLevels = [];
+for(let i = 1; i <= maxLevel; i++){
+	const key = `spell${i}`;
+	if(spells[key].value < spells[key].max && spells[key].max > 0){
+		validLevels.push(key);
+	}
+}
+const {level: pl, max: pm, value: pv} = spells["pact"];
+if(pm > 0 && pl <= maxLevel && pv < pm) validLevels.push("pact");
 
-// reached spell levels
-const lvl = Array.fromRange(Math.ceil(prof/2)).map(i => i+1).filter(i => spells[`spell${i}`].max > 0);
-
-// maps of values and max values
-const val = lvl.map(i => spells[`spell${i}`].value);
-const max = lvl.map(i => spells[`spell${i}`].max);
-
-// bail out of you are not missing any spell slots
-if(lvl.filter(i => spells[`spell${i}`].value !== spells[`spell${i}`].max).length < 1) return ui.notifications.error("You are not missing any valid spell slots.");
-
-// expend use of feature
-const roll = await item.roll();
-if(!roll) return;
-
-// helper variables for the dialog
-const maxVal = 1;
-let spent = 0;
+// bail out if you are not missing any spell slots or if the item has no remaining uses (limited uses and resources).
+if(!validLevels.length) return ui.notifications.warn("You are not missing any valid spell slots.");
+const {value: limUses} = item.data.data.uses;
+if(!limUses) return ui.notifications.warn("You have no uses left of Harness Divine Power.");
+const resourceItem = actor.items.get(item.data.data.consume.target);
+const {value: conUses} = resourceItem.data.data.uses;
+if(!conUses) return ui.notifications.warn("You have no uses of Channel Divinity left.");
 
 // define dialog contents
-let content = `<div name="header"><p>Recovering spell slots: <strong>${spent}</strong> / ${maxVal}.</p></div><table>`;
-for(let i = 0; i < lvl.length; i++){
-	content += `<tr><td style="text-align: center"><strong>${nth(i+1)}-level:</strong></td><td>`;
-	for(let j = 0; j < max[i]; j++) content += `<input type="checkbox" value="${i+1}" name="level${lvl[i]}" ${j < val[i] ? 'checked disabled' : ''}/>`;
-	content += `</td></tr>`;
-}
-content += `</table>`;
+const options = validLevels.reduce((acc, e) => {
+	const {value, max} = spells[e];
+	const label = e === "pact" ? "Pact Slots" : (nth(Number(e.at(-1))) + " Level");
+	return acc + `<option value="${e}">${label} (${value} / ${max})</option>`;
+}, ``);
 
 new Dialog({
 	title: "Harness Divine Power",
-	content,
+	content: `
+	<p>Select a spell slot level.</p>
+	<form>
+		<div class="form-group">
+			<label>Spell Slot:</label>
+			<div class="form-fields">
+				<select id="hdp-level">${options}</select>
+			</div>
+		</div>
+	</form>`,
 	buttons: {
-		ok: {
-			icon: `<i class="fas fa-check"></i>`,
+		run: {
+			icon: `<i class="fas fa-hand-sparkles"></i>`,
 			label: "Recover",
 			callback: async (html) => {
-				if(spent > maxVal || spent === 0) return ui.notifications.error("Invalid number of slots to recover.");
-				const obj = {};
-				for(let i = 1; i <= 9; i++) obj[`data.spells.spell${i}.value`] = html[0].querySelectorAll(`input[name=level${i}]:checked`).length;
-				await actor.update(obj);
-				return ui.notifications.info("Spell slots recovered!");
+				const level = html[0].querySelector("#hdp-level").value;
+				spells[level].value++;
+				await actor.update({"data.spells": spells});
+				await actor.updateEmbeddedDocuments("Item", [
+					{_id: resourceItem.id, "data.uses.value": conUses - 1},
+					{_id: item.id, "data.uses.value": limUses - 1}
+				]);
+				return ui.notifications.info("Recovered a spell slot!");
 			}
 		}
-	},
-	render: (html) => {
-		html[0].querySelectorAll("input[type=checkbox]").forEach(input => {
-			input.addEventListener("change", function(){
-				spent = lvl.reduce((acc, e) => acc += Number(html[0].querySelectorAll(`input[name=level${e}]:checked:not(:disabled)`).length), 0);
-				html[0].querySelector("div[name=header]").innerHTML = `<p>Recovering spell slots: <strong>${spent}</strong> / ${maxVal}.</p>`;
-			});
-		});
-	},
-	default: "ok"
+	}
 }).render(true);
 
 function nth(n){return n + (["st","nd","rd"][((n+90)%100-10)%10-1]||"th")}
