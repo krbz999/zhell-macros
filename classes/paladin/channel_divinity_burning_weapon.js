adfde4d041bf83f94659c5daccc8d84701926eef
Zhell <50169243+krbz999@users.noreply.github.com>
2022-09-20 22:14:02 +0200

    Update channel_divinity_burning_weapon.js

--- classes/paladin/channel_divinity_burning_weapon.js
+++ classes/paladin/channel_divinity_burning_weapon.js
@@ -11,57 +11,73 @@
 
 const name = "Burning Weapon";
 const hasMutation = warpgate.mutationStack(token.document).getName(name);
-if(hasMutation) return warpgate.revert(token.document, name);
+if ( hasMutation ) return warpgate.revert(token.document, name);
 
-const weapons = actor.itemTypes.weapon.filter(i => i.data.data.equipped);
-if(weapons.length < 1) return ui.notifications.warn("You have no equipped weapons.");
+const weapons = actor.itemTypes.weapon.filter(i => i.system.equipped);
+if ( !weapons.length ) return ui.notifications.warn("You have no equipped weapons.");
 
-const roll = await item.roll();
-if(!roll) return;
+const use = await item.use();
+if ( !use ) return;
 
 // mutation function:
-const mutate = async (weaponId) => {
-	const weapon = actor.items.get(weaponId);
-	if(!weapon) return;
-	const mod = Math.max(1, actor.data.data.abilities.cha.mod);
-	const damageParts = weapon.data.data.damage.parts;
-	damageParts[0][0] = `${damageParts[0][0]} + ${mod}[fire]`;
-	
-	await warpgate.mutate(token.document, {
-		token: {light: {bright: 20, dim: 40, color: "#e05d06", "animation.type": "torch"}},
-		embedded: {Item: {[weaponId]: {data: {"properties.mgc": true, "damage.parts": damageParts}}}}
-	}, {}, {name, comparisonKeys: {Item: "_id"}});
+async function mutate(weaponId){
+    const weapon = actor.items.get(weaponId);
+    if ( !weapon ) return;
+    const mod = Math.max(1, actor.system.abilities.cha.mod);
+    const damageParts = weapon.system.damage.parts;
+    damageParts[0][0] = `${damageParts[0][0]} + ${mod}[fire]`;
+    
+    await warpgate.mutate(token.document, {
+        token: {
+            light: {
+                bright: 20,
+                dim: 40,
+                color: "#e05d06",
+                "animation.type": "torch"
+            }
+        },
+        embedded: {
+            Item: {
+                [weaponId]: {
+                    system: {
+                        "properties.mgc": true,
+                        "damage.parts": damageParts
+                    }
+                }
+            }
+        }
+    }, {}, { name, comparisonKeys: { Item: "_id" } });
 }
 
 /* exactly one weapon */
-if(weapons.length === 1) return mutate(weapons[0].id);
+if ( weapons.length === 1 ) return mutate(weapons[0].id);
 
 /* multiple weapons */
-const weaponSelect = weapons.reduce((acc, {id, name}) => acc += `<option value="${id}">${name}</option>`, ``);
+const weaponSelect = weapons.reduce((acc, { id, name }) => {
+    return acc + `<option value="${id}">${name}</option>`;
+}, "");
 const content = `
-	<p>Pick your weapon for ${name}.</p>
-	<form>
-		<div class="form-group">
-			<label for="wpn">Weapon</label>
-			<div class="form-fields">
-				<select id="wpn">${weaponSelect}</select>
-			</div>
-		</div>
-	</form>`;
+<p>Pick your weapon for ${name}.</p>
+<form>
+    <div class="form-group">
+        <label for="wpn">Weapon</label>
+        <div class="form-fields">
+            <select id="wpn">${weaponSelect}</select>
+        </div>
+    </div>
+</form>`;
 
 new Dialog({
-	title: name,
-	content,
-	buttons: {
-		go: {
-			icon: `<i class="fas fa-check"></i>`,
-			label: "Flame On!",
-			callback: (html) => {
-				const weaponId = html[0].querySelector("select[id=wpn]").value;
-				return mutate(weaponId);
-			}
-		}
-	},
-	default: "go",
+    title: name,
+    content,
+    buttons: {
+        go: {
+            icon: "<i class='fas fa-fire'></i>",
+            label: "Flame On!",
+            callback: (html) => {
+                const id = html[0].querySelector("#wpn").value;
+                return mutate(id);
+            }
+        }
+    }
 }).render(true);
-
