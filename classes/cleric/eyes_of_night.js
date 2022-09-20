388b259f0cc99602def3a7e2b8c7a65e85a174c6
Zhell <50169243+krbz999@users.noreply.github.com>
2022-09-20 22:27:19 +0200

    Update eyes_of_night.js

--- classes/cleric/eyes_of_night.js
+++ classes/cleric/eyes_of_night.js
@@ -1,14 +1,29 @@
 // EYES OF NIGHT
-// Required modules: warpgate, itemacro.
+// Required modules: warpgate, itemacro
 
 const range = 120;
 
-const mod = Math.max(actor.getRollData().abilities.wis.mod, 1);
+const mod = Math.max(actor.system.abilities.wis.mod, 1);
 const size = game.user.targets.size;
-if(mod < size || size < 1) return ui.notifications.error("Please target an appropriate number of creatures.");
-const updates = {actor: {"data.attributes.senses.darkvision": range}, token: {dimSight: range}};
-const options = {name: `Darkvision (${range}ft)`, description: `You are being granted ${range} feet of darkvision.`};
+if ( mod < size || size < 1 ) {
+    ui.notifications.error("Please target an appropriate number of creatures.");
+    return;
+}
+const updates = {
+    actor: {
+        "system.attributes.senses.darkvision": range
+    },
+    token: {
+        sight: { dim: range }
+    }
+}
+const options = {
+    name: `Darkvision (${range}ft)`,
+    description: `You are being granted ${range} feet of darkvision.`
+}
 
-const roll = await item.roll();
-if(!roll) return;
-for(let target of game.user.targets) await warpgate.mutate(target.document, updates, {}, options);
+const use = await item.use();
+if ( !use ) return;
+for ( const target of game.user.targets) {
+    await warpgate.mutate(target.document, updates, {}, options);
+}
