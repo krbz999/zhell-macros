c580d3494211dc6f326d5ae24c2890f96d0ee254
Zhell <50169243+krbz999@users.noreply.github.com>
2022-09-28 20:24:18 +0200

    Update scroll_to_spell.js
    v10

--- classes/wizard/scroll_to_spell.js
+++ classes/wizard/scroll_to_spell.js
@@ -4,35 +4,42 @@
 const key = "..."; // key for the pack where you find the spell.
 const wizard = game.actors.getName("Name of the wizard");
 
-const scrolls = wizard.itemTypes.consumable.filter(i => i.data.data.consumableType === "scroll" && i.name.includes("Spell Scroll:"));
-if(scrolls.length < 1) return ui.notifications.info("Actor has no scrolls.");
-const options = scrolls.reduce((acc, {id, name}) => acc += `<option value="${id}">${name}</option>`, ``);
+const scrolls = wizard.itemTypes.consumable.filter(item => {
+  if ( item.system.consumableType !== "scroll" ) return false;
+  return item.name.startsWith("Spell Scroll:");
+});
+if ( !scrolls.length ) {
+  ui.notifications.info("Actor has no scrolls.");
+  return;
+}
+const options = scrolls.reduce((acc, { id, name }) => {
+  return acc += `<option value="${id}">${name}</option>`;
+}, "");
 const content = `
-	<form>
-		<div class="form-group">
-			<label for="item-select">Select the scroll:</label>
-			<div class="form-fields">
-				<select id="item-select">${options}</select>
-			</div>
-		</div>
-	</form>`;
+<form>
+  <div class="form-group">
+    <label for="item-select">Select the scroll:</label>
+    <div class="form-fields">
+      <select id="item-select">${options}</select>
+    </div>
+  </div>
+</form>`;
 new Dialog({
-	title: "Scroll to Spell",
-	content,
-	buttons: {
-		go: {
-			icon: `<i class="fas fa-check"></i>`,
-			label: "Create Spell",
-			callback: async (html) => {
-				const scrollId = html[0].querySelector("select[id='item-select']").value;
-				const scroll = wizard.items.get(scrollId);
-				const pack = await game.packs.get(key).getIndex();
-				const spellId = pack.find(i => i.name === scroll.name.replace("Spell Scroll: ", ""))?._id;
-				const spell = await game.packs.get(key).getDocument(spellId);
-				const createdSpell = await wizard.createEmbeddedDocuments("Item", [spell?.toObject()]);
-				if(createdSpell.length > 0) scroll?.delete();
-			}
-		}
-	},
-	default: "go"
+  title: "Scroll to Spell",
+  content,
+  buttons: {
+    go: {
+      icon: "<i class='fa-solid fa-check'></i>",
+      label: "Create Spell",
+      callback: async (html) => {
+        const scrollId = html[0].querySelector("#item-select").value;
+        const scroll = wizard.items.get(scrollId);
+        const pack = game.packs.get(key);
+        const spellId = pack.index.getName(scroll.name.replace("Spell Scroll: ", ""))._id;
+        const spell = pack.getDocument(spellId);
+        const createdSpell = await wizard.createEmbeddedDocuments("Item", [spell.toObject()]);
+        if ( createdSpell.length > 0 ) return scroll.delete();
+      }
+    }
+  }
 }).render(true);
