57b3a50040a053ee9d0fec7948c689c56ab21692
Zhell <50169243+krbz999@users.noreply.github.com>
2022-09-28 20:29:55 +0200

    Update bladesong.js
    v10

--- classes/wizard/bladesong.js
+++ classes/wizard/bladesong.js
@@ -7,36 +7,33 @@
 // required modules: itemacro
 // added benefit with concentrationnotifier and convenientDescription.
 
-const effect = actor.effects.find(i => i.getFlag("world", "bladesong") === actor.id);
-if(effect) return effect.delete();
+const effect = actor.effects.find(e => e.getFlag("world", "bladesong") === actor.id);
+if ( effect ) return effect.delete();
 
-const {abilities: {int: {mod}}, classes: {wizard: {levels}}} = duplicate(actor.getRollData());
-const value = `+${mod}`;
-const mode = CONST.ACTIVE_EFFECT_MODES.ADD;
-
+const {
+  abilities: { int: { mod } },
+  classes: { wizard: { levels } }
+} = actor.getRollData();
+const {ADD} = CONST.ACTIVE_EFFECT_MODES;
 const changes = [
-	{key: "data.attributes.ac.bonus", mode, value},
-	{key: "data.attributes.movement.walk", mode, value: "+10"},
-	{key: "data.skills.acr.bonuses.passive", mode, value: "+5"},
-	{key: "flags.dnd5e.concentrationBonus", mode, value}
+  { key: "system.attributes.ac.bonus", mode: ADD, value: mod },
+  { key: "system.attributes.movement.walk", mode: ADD, value: 10 },
+  { key: "flags.dnd5e.concentrationBonus", mode: ADD, value: `+${mod}` }
 ];
-if(levels >= 14) changes.push({key: "data.bonuses.mwak.damage", mode, value});
+if ( levels >= 14 ) changes.push({ key: "system.bonuses.mwak.damage", mode: ADD, value: `+${mod}` });
 
-
-
-const roll = await item.roll();
-if(!roll) return;
-
+const use = await item.use();
+if ( !use ) return;
 await actor.createEmbeddedDocuments("ActiveEffect", [{
-	changes,
-	icon: item.img,
-	label: item.name,
-	duration: {seconds: 60},
-	"flags.world.bladesong": actor.id,
-	"flags.convenientDescription": `
-		You have ${value} to AC,
-		+10ft movement speed,
-		advantage on Acrobatics,
-		${levels >= 14 ? value + " to melee weapon damage," : ""}
-		and ${value} to saving throws for concentration.`,
+  changes,
+  icon: item.img,
+  label: item.name,
+  duration: { seconds: 60 },
+  "flags.world.bladesong": actor.id,
+  "flags.visual-active-effects.data": `
+    You have +${mod} to AC,
+    +10ft movement speed,
+    advantage on Acrobatics,
+    ${levels >= 14 ? "+" + mod + " to melee weapon damage," : ""}
+    and +${mod} to saving throws for concentration.`,
 }]);
