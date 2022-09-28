9ae0ec7228b678da002aaa26bfe5973eed7dd92b
Zhell <50169243+krbz999@users.noreply.github.com>
2022-09-28 20:17:42 +0200

    Update summon_tentacle.js
    v10

--- classes/warlock/summon_tentacle.js
+++ classes/warlock/summon_tentacle.js
@@ -1,31 +1,25 @@
 // Fathomless Warlock, spawn Fathomless Tentacle.
-// if spawned actor already exists, roll its attack (a feature on the spawn with the same name).
+// if spawned actor already exists, show this item's chat card, otherwise summon.
 // required modules: warpgate, itemacro.
 
-// check if spawn already exists, and if it does, roll the item.
+// check if spawn already exists, and if it does, use this item.
 const spawn = canvas.scene.tokens.find(t => t.actor.getFlag("world", "fathomless-tentacle") === actor.id);
-if(!!spawn) return spawn.actor.items.getName("Fathomless Tentacle").roll();
+if ( spawn ) return item.displayCard();
 
 // if spawn does not exist, cast the spell / use the feature:
-const roll = await item.roll();
-if(!roll) return;
+const use = await item.use();
+if ( !use ) return;
 
 // updates to the spawn's token, actor, and items:
 const updates = {
-	token: {
-		name: `${game.user.charname}'s Fathomless Tentacle`,
-		displayName: CONST.TOKEN_DISPLAY_MODES.HOVER,
-		displayBars: CONST.TOKEN_DISPLAY_MODES.NONE
-	},
-	actor: {
-		"data.abilities": actor.getRollData().abilities,
-		"data.details.cr": actor.data.data.details.level,
-		"flags.world.fathomless-tentacle": actor.id
-	},
-	embedded: {Item: {"Fathomless Tentacle": {"data.damage.parts": [[
-		Roll.replaceFormulaData(item.data.data.damage.parts[0][0], actor.getRollData()),
-		item.data.data.damage.parts[0][1]
-	]]}}}
+  token: {
+    name: `${game.user.charname}'s Fathomless Tentacle`,
+    displayName: CONST.TOKEN_DISPLAY_MODES.HOVER,
+    displayBars: CONST.TOKEN_DISPLAY_MODES.NONE
+  },
+  actor: {
+    "flags.world.fathomless-tentacle": actor.id
+  }
 }
 
 // now spawn the actor:
