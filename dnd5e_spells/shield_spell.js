1f37eb19c7e6ec1cb2a728b4b2a96fb5cf83b1ad
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-13 17:10:03 +0200

    v10 update

--- dnd5e_spells/shield_spell.js
+++ dnd5e_spells/shield_spell.js
@@ -1,20 +1,24 @@
-/* Shield spell
-- click while no effect active: cast and apply +5 to AC
-- click while effect active: remove effect
-
-Required modules: itemacro
-*/
+// SHIELD
+// required modules: itemacro.
+// supported: visual-active-effects
 
 const effect = actor.effects.find(i => i.getFlag("world", "shield-spell") === actor.id);
-if(effect) return effect.delete();
+if (effect) return effect.delete();
 
-const roll = await item.roll();
-if(!roll) return;
+const use = await item.use();
+if (!use) return;
 await actor.createEmbeddedDocuments("ActiveEffect", [{
-	changes: [{key: "data.attributes.ac.bonus", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: "+5"}],
-	icon: item.img,
-	label: item.name,
-	duration: {rounds: 1},
-	"flags.world.shield-spell": actor.id,
-	"flags.convenientDescription": "You have +5 to AC and immunity to damage from Magic Missile."
+  changes: [{
+    key: "system.attributes.ac.bonus",
+    mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+    value: 5
+  }],
+  icon: item.img,
+  label: item.name,
+  duration: { rounds: 1 },
+  "flags.world.shield-spell": actor.id,
+  "flags.visual-active-effects.data": {
+    intro: "You have +5 to AC and immunity to damage from Magic Missile.",
+    content: item.system.description.value
+  }
 }]);
