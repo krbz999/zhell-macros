d48387c3c17db4e261dd2a01a504aa782716884b
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-13 16:56:57 +0200

    Update hook_remove_initiative_rolls.js

--- dnd5e_utils/hook_remove_initiative_rolls.js
+++ dnd5e_utils/hook_remove_initiative_rolls.js
@@ -1,14 +1,9 @@
 // delete all initiative rolls from chat. Only show 3d dice rolls for player characters.
 Hooks.on("preCreateChatMessage", (msg, roll, context, userId) => {
-	// pre-hook, so don't need to filter by user, everyone just marks their messages as temp.
-	
-	// bail out if not initiative roll.
-	if(!msg.getFlag("core", "initiativeRoll")) return;
-	
-	// set message as temporary.
-	context.temporary = true;
-	
-	// show 3d dice if player-owned actor.
-	const actor = game.actors.get(roll.speaker.actor);
-	if(!!actor && actor.hasPlayerOwner) game.dice3d?.showForRoll(msg.roll, game.user, true);
+  if (!msg.getFlag("core", "initiativeRoll")) return;
+  context.temporary = true;
+  const actor = game.actors.get(roll.speaker.actor);
+  if (!!actor && actor.hasPlayerOwner) {
+    game.dice3d?.showForRoll(msg.roll, game.user, true);
+  }
 });
