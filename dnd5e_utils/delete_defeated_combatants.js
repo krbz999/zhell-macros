3bcb6f0e90d7821c8b43ce9e6e4b8ab137c3ac84
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-13 16:48:21 +0200

    Update delete_defeated_combatants.js

--- dnd5e_utils/delete_defeated_combatants.js
+++ dnd5e_utils/delete_defeated_combatants.js
@@ -1,14 +1,9 @@
 // delete combatants from the combat tracker if and only if they have 0 or less hp and do not have a player owner.
 // required modules: none.
 
-// the ids of the combatants to delete.
-const ids = game.combat.combatants.filter(i => {
-  // filter out player-owned combatants.
-  if(i.hasPlayerOwner) return false;
-  // filter out living combatants
-  if(getProperty(i.token, "actor.data.data.attributes.hp.value") > 0) return false;
-  return true;
-}).map(i => i.id);
-
-// yeet 'em.
+const ids = game.combat.combatants.filter(c => {
+  const player = c.token.actor.hasPlayerOwner;
+  const hp = c.token.actor.system.attributes.hp.value > 0;
+  return !player && !hp;
+}).map(c => c.id);
 await game.combat.deleteEmbeddedDocuments("Combatant", ids);
