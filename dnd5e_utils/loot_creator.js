3c7c8e7a4a182eead6568f587f889d73ddfac5a5
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-13 17:01:30 +0200

    Update loot_creator.js

--- dnd5e_utils/loot_creator.js
+++ dnd5e_utils/loot_creator.js
@@ -10,77 +10,98 @@ const {files} = await FilePicker.browse("public", "icons/commodities/treasure");
 const img = files[Math.floor(Math.random() * files.length)];
 
 // set up input fields.
-const nameField = `<input id="name" type="text" value="Curiosity"></input>`;
-const priceField = `<input id="price" type="number" value="25"></input>`;
-const weightField = `<input id="weight" type="number" value="1"></input>`;
-const quantityField = `<input id="quantity" type="number" value="1"></input>`;
+const nameField = `<input id="name" type="text" value="Curiosity">`;
+const priceField = `<input id="price" type="number" value="25">`;
+const weightField = `<input id="weight" type="number" value="1">`;
+const quantityField = `<input id="quantity" type="number" value="1">`;
 const descriptionField = `<textarea style="resize: none" id="description" placeholder="Enter text..."></textarea>`;
-const sidebarCheck = `<input type="checkbox" id="sidebarbox"></input>`;
+const sidebarCheck = `<input type="checkbox" id="sidebarbox">`;
 
 // set up html.
 const content = `
-  <p style="text-align:center"><img src="${img}" style="width:60px; height: 60px;"></p>
-  <hr>
-  <form>
-    <div class="form-group">
-      <label for="name">Item name</label>
-      <div class="form-fields">${nameField}</div>
-    </div>
-    <div class="form-group">
-      <label for="price">Price (GP)</label>
-      <div class="form-fields">${priceField}</div>
-    </div>
-    <div class="form-group">
-      <label for="weight">Weight (lbs.)</label>
-      <div class="form-fields">${weightField}</div>
-    </div>
-    <div class="form-group">
-      <label for="quantity">Quantity</label>
-      <div class="form-fields">${quantityField}</div>
-    </div>
-    <div class="form-group">
-      <label for="description">Description</label>
-      <div class="form-fields">${descriptionField}</div>
-    </div>
-    <div class="form-group">
-      <label for="sidebarbox">Add to sidebar</label>
-      <div class="form-fields">${sidebarCheck}</div>
-    </div>
-  </form>
-  <hr>`;
+<p style="text-align:center"><img src="${img}" style="width:60px; height: 60px;"></p>
+<hr>
+<form>
+  <div class="form-group">
+    <label for="name">Item name</label>
+    <div class="form-fields">${nameField}</div>
+  </div>
+  <div class="form-group">
+    <label for="price">Price (GP)</label>
+    <div class="form-fields">${priceField}</div>
+  </div>
+  <div class="form-group">
+    <label for="weight">Weight (lbs.)</label>
+    <div class="form-fields">${weightField}</div>
+  </div>
+  <div class="form-group">
+    <label for="quantity">Quantity</label>
+    <div class="form-fields">${quantityField}</div>
+  </div>
+  <div class="form-group">
+    <label for="description">Description</label>
+    <div class="form-fields">${descriptionField}</div>
+  </div>
+  <div class="form-group">
+    <label for="sidebarbox">Add to sidebar</label>
+    <div class="form-fields">${sidebarCheck}</div>
+  </div>
+</form>`;
 
 // create and render dialog.
-new Dialog({content, title: "Create Loot", buttons: {go: {
-  icon: `<i class="fas fa-check"></i>`,
-  label: "Create Loot!",
-  callback: async (html) => {
-    // construct item data.
-    const name = html[0].querySelector("#name").value;
-    const value = html[0].querySelector("#description").value;
-    const price = Number(html[0].querySelector("#price").value);
-    const quantity = Number(html[0].querySelector("#quantity").value);
-    const weight = Number(html[0].querySelector("#weight").value);
-    
-    const itemData = {name, img, type: "loot", system: {description: {value}, price, quantity, rarity: "common", weight}}
-    
-    // pick the target or location.
-    const crosshairs = await warpgate.crosshairs.show({label: "Select recipient or location", drawIcon: false, size: 1});
-    const tokenDocs = !crosshairs.cancelled ? warpgate.crosshairs.collect(crosshairs) : [];
-    
-    // pop it in the sidebar
-    const sidebar = html[0].querySelector("#sidebarbox").checked;
-    if (sidebar) await Item.createDocuments([itemData]);
-    
-    // if no token was targeted, add the item to a new item pile, initially hidden.
-    if (!tokenDocs.length) {
-      const updates = {embedded: {Item: {[name]: itemData}}, token: {hidden: true}}
-      return warpgate.spawnAt(crosshairs, "Default Item Pile", updates);
+new Dialog({
+  content,
+  title: "Create Loot",
+  buttons: {
+    go: {
+      icon: "<i class='fa-solid fa-check'></i>",
+      label: "Create Loot!",
+      callback: async (html) => {
+        // construct item data.
+        const name = html[0].querySelector("#name").value;
+        const value = html[0].querySelector("#description").value;
+        const price = Number(html[0].querySelector("#price").value);
+        const quantity = Number(html[0].querySelector("#quantity").value);
+        const weight = Number(html[0].querySelector("#weight").value);
+        const itemData = [{
+          name,
+          img,
+          type: "loot",
+          system: {
+            description: { value },
+            price,
+            quantity,
+            rarity: "common",
+            weight
+          }
+        }];
+        // pick the target or location.
+        const crosshairs = await warpgate.crosshairs.show({
+          label: "Select recipient or location",
+          drawIcon: false,
+          size: 1
+        });
+        const tokenDocs = !crosshairs.cancelled ? warpgate.crosshairs.collect(crosshairs) : [];
+        // pop it in the sidebar
+        const sidebar = html[0].querySelector("#sidebarbox").checked;
+        if (sidebar) await Item.createDocuments(itemData);
+        
+        // if no token was targeted, add the item to a new item pile, initially hidden.
+        if (!tokenDocs.length) {
+          const updates = {
+            embedded: {
+              Item: { [name]: itemData[0] }
+            },
+            token: { hidden: true }
+          }
+          return warpgate.spawnAt(crosshairs, "Default Item Pile", updates);
+        }
+        // if a token was targeted, add to their inventory instead.
+        await tokenDocs[0].actor.createEmbeddedDocuments("Item", itemData);
+        // if single item pile it's probably named as that item, so fix that
+        const isItemPile = tokenDocs[0].getFlag("item-piles", "data.enabled");
+        if (!!isItemPile) await tokenDocs[0].update({ name: "Pile of Loot" });
+      }
     }
-    
-    // if a token was targeted, add to their inventory instead.
-    await tokenDocs[0].actor.createEmbeddedDocuments("Item", [itemData]);
-    // if single item pile it's probably named as that item, so fix that 
-    const isItemPile = tokenDocs[0].getFlag("item-piles", "data.enabled");
-    if(!!isItemPile) await tokenDocs[0].update({name: "Pile of Loot"});
   }
-}}, default: "go"}).render(true);
+}).render(true);
