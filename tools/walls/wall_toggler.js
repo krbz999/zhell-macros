8bf78ad628ec4196caa4880fda56152393a5801e
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-14 04:26:34 +0200

    v10 update

--- tools/walls/wall_toggler.js
+++ tools/walls/wall_toggler.js
@@ -1,17 +1,19 @@
 // execute macro to cycle selected wall directions (both, left, right).
 // hold shift to instead cycle between movement types (none, normal).
 
-if(!event.shiftKey){
-  const {BOTH, LEFT, RIGHT} = CONST.WALL_DIRECTIONS;
-  const updates = canvas.walls.controlled.map(i => {
+if (!event.shiftKey ){
+  const { BOTH, LEFT, RIGHT } = CONST.WALL_DIRECTIONS;
+  const updates = canvas.walls.controlled.map(wall => {
     let dir = BOTH;
-    if(i.document.data.dir === BOTH) dir = LEFT;
-    else if(i.document.data.dir === LEFT) dir = RIGHT;
-    return {_id: i.id, dir}
+    if (wall.document.dir === BOTH) dir = LEFT;
+    else if (wall.document.dir === LEFT) dir = RIGHT;
+    return { _id: wall.id, dir };
   });
   await canvas.scene.updateEmbeddedDocuments("Wall", updates);
-}else{
-  const {NONE, NORMAL} = CONST.WALL_MOVEMENT_TYPES;
-  const updates = canvas.walls.controlled.map(i => ({_id: i.id, move: i.document.data.move === NONE ? NORMAL : NONE}));
+} else {
+  const { NONE, NORMAL } = CONST.WALL_MOVEMENT_TYPES;
+  const updates = canvas.walls.controlled.map(wall => {
+    return { _id: wall.id, move: wall.document.move === NONE ? NORMAL : NONE };
+  });
   await canvas.scene.updateEmbeddedDocuments("Wall", updates);
 }
