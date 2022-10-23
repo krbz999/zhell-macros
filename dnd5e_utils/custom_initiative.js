9c42a043bff304fd8198d53a66586f555b3486b5
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-23 11:49:06 +0200

    Temporary fix

--- dnd5e_utils/custom_initiative.js
+++ dnd5e_utils/custom_initiative.js
@@ -9,49 +9,63 @@
 
 
 const action = async () => {
-    const selected = canvas.tokens.controlled;
-    const charTokens = game.user.character?.getActiveTokens() ?? [];
-    const itemPilesActive = !!game.modules.get("item-piles")?.active;
-    let toks = selected.length > 0 ? selected : charTokens;
+  const selected = canvas.tokens.controlled;
+  const charTokens = game.user.character?.getActiveTokens() ?? [];
+  const itemPilesActive = !!game.modules.get("item-piles")?.active;
+  let toks = selected.length > 0 ? selected : charTokens;
 
-    toks = toks.filter(i => {
-        if ( i.combatant && i.combatant.initiative !== null ) return false;
-        if ( !itemPilesActive ) return true;
-        if ( i.actor.getFlag("item-piles", "data.active") ) return false;
-        return true;
-    });
-    if( !toks.length ){
-        return ui.notifications.warn("No valid tokens.");
+  toks = toks.filter(i => {
+    if (i.combatant && i.combatant.initiative !== null) return false;
+    if (!itemPilesActive) return true;
+    if (i.actor.getFlag("item-piles", "data.active")) return false;
+    return true;
+  });
+  if (!toks.length) {
+    return ui.notifications.warn("No valid tokens.");
+  }
+  const updates = [];
+  for (const tok of toks) {
+    const parts = [];
+    // init bonuses.
+    const init_only = tok.actor.getFlag("world", "initiative-bonus");
+    if (!!init_only) parts.push(init_only);
+    parts.push(tok.actor.system.attributes.init.value);
+
+    // the following is only until the dnd5e issue gets fixed.
+    // dex mod
+    parts.push("@mod");
+    // prof
+    const abl = tok.actor.system.abilities["dex"];
+    if (abl.checkProf.hasProficiency) parts.push("@prof");
+    // dex check bonuses
+    if (abl.bonuses.check) parts.push("@dexCheckBonus");
+    // global actor bonus
+    if (tok.actor.system.bonuses?.abilities?.check) parts.push("@checkBonus");
+    // end.
+
+    const rollOptions = {
+      chatMessage: !game.user.isGM,
+      event,
+      parts,
+      advantage: !!tok.actor.getFlag("dnd5e", "initiativeAdv"),
+      disadvantage: !!tok.actor.getFlag("dnd5e", "initiativeDisadv")
     }
-    const updates = [];
-    for ( let tok of toks ) {
-        const parts = [];
-        const init_only = tok.actor.getFlag("world", "initiative-bonus");
-        if( !!init_only ) parts.push(init_only);
-        parts.push(tok.actor.system.attributes.init.value);
-        const rollOptions = {
-            chatMessage: !game.user.isGM,
-            event,
-            parts,
-            advantage: !!tok.actor.getFlag("dnd5e", "initiativeAdv"),
-            disadvantage: !!tok.actor.getFlag("dnd5e", "initiativeDisadv")
-        }
-        const {total} = await tok.actor.rollAbilityTest("dex", rollOptions);
-        if ( total !== undefined && total !== null ) {
-            if ( !tok.inCombat ) await tok.toggleCombat();
-            updates.push({ _id: tok.combatant.id, initiative: total });
-        }
+    const { total } = await tok.actor.rollAbilityTest("dex", rollOptions);
+    if (total !== undefined && total !== null) {
+      if (!tok.inCombat) await tok.toggleCombat();
+      updates.push({ _id: tok.combatant.id, initiative: total });
     }
-    return game.combats.get(this.combatId).updateEmbeddedDocuments("Combatant", updates);
+  }
+  return game.combats.get(this.combatId).updateEmbeddedDocuments("Combatant", updates);
 }
 
 const combat = game.combat ?? await Combat.create({ scene: canvas.scene.id, active: true });
 await Requestor.request({
-    buttonData: [{ action, label: "Roll Initiative!", combatId: combat.id }],
-    img: "icons/skills/melee/weapons-crossed-swords-yellow.webp",
-    description: "Roll initiative for all selected tokens or your assigned character.",
-    title: "Roll Initiative",
-    limit: Requestor.LIMIT.FREE,
-    context: { popout: true, autoClose: true },
-    speaker: ChatMessage.getSpeaker({alias: "Initiative"})
+  buttonData: [{ action, label: "Roll Initiative!", combatId: combat.id }],
+  img: "icons/skills/melee/weapons-crossed-swords-yellow.webp",
+  description: "Roll initiative for all selected tokens or your assigned character.",
+  title: "Roll Initiative",
+  limit: Requestor.LIMIT.FREE,
+  context: { popout: true, autoClose: true },
+  speaker: ChatMessage.getSpeaker({ alias: "Initiative" })
 });
