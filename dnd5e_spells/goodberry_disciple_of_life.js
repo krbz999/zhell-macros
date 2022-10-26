6d2b9de6f79564d96c80855aba600ef64747f5be
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-26 23:44:15 +0200

    v10 update

--- dnd5e_spells/goodberry_disciple_of_life.js
+++ dnd5e_spells/goodberry_disciple_of_life.js
@@ -2,38 +2,51 @@
 // required modules: itemacro.
 
 // whether or not actor is also a Life Cleric.
-const disciple_of_life = true;
+const disciple = true;
 
-
-const item = actor.itemTypes.spell.find(i => i.name === "Goodberry");
+const item = actor.itemTypes.spell.find(i => {
+  return i.name === "Goodberry";
+});
 
 // get spell level;
-const roll = await item.roll();
-if(!roll) return;
-const content = roll.data.content;
-const level = Number(content.charAt(content.indexOf("data-spell-level") + 18));
-const bonus = disciple_of_life ? level + 2 : 0;
-const name = disciple_of_life ? `Goodberry (${nth(level)})` : "Goodberry";
+const use = await item.use();
+if (!use) return;
+const DIV = document.createElement("DIV");
+DIV.innerHTML = use.content;
+const level = Number(DIV.firstChild.dataset.spellLevel);
+const bonus = disciple ? level + 2 : 0;
+const name = disciple ? `Goodberry (${nth(level)})` : "Goodberry";
 
 // find existing goodberry
-const existingItem = actor.itemTypes.consumable.find(i => i.name === name);
-if(!!existingItem) await existingItem.update({"data.quantity": existingItem.data.data.quantity + 10});
+const existingItem = actor.itemTypes.consumable.find(i => {
+  return i.name === name;
+});
+if (existingItem) {
+  const quantity = existingItem.system.quantity + 10;
+  return existingItem.update({ "system.quantity": quantity });
+}
 
 // else create new goodberry stack
-else await actor.createEmbeddedDocuments("Item", [{
-	name,
-	type: "consumable",
-	img: "icons/consumables/food/berries-ration-round-red.webp",
-	data: {
-		damage: {parts: [[`${1 + bonus}`, "healing"]]},
-		quantity: 10,
-		description: {value: `<p>You can use an action to eat a goodberry to restore ${1 + bonus} hit point(s).</p>`},
-		rarity: "common",
-		activation: {type: "action", cost: 1},
-		uses: {value: 1, max: 1, per: "charges", autoDestroy: true},
-		actionType: "heal",
-		consumableType: "food"
-	}
+return actor.createEmbeddedDocuments("Item", [{
+  name,
+  type: "consumable",
+  img: "icons/consumables/food/berries-ration-round-red.webp",
+  system: {
+    damage: { parts: [[`${1 + bonus}`, "healing"]] },
+    quantity: 10,
+    description: {
+      value: `<p>You can use an action to eat a goodberry to restore ${1 + bonus} hit point(s).</p>`
+    },
+    rarity: "common",
+    activation: {
+      type: "action", cost: 1
+    },
+    uses: {
+      value: 1, max: 1, per: "charges", autoDestroy: true
+    },
+    actionType: "heal",
+    consumableType: "food"
+  }
 }]);
 
 /* helper function */
