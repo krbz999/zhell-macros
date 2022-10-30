8ea03afcfca05df3eb1fde67b394b8d23ed6e214
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-30 16:57:43 +0100

    Update show_image_from_url.js

--- tools/show_image_from_url.js
+++ tools/show_image_from_url.js
@@ -1,27 +1,16 @@
 // shows the provided media to all players.
 
-new Dialog({
+await Dialog.prompt({
   title: "Share Image via URL",
-  content: `
-  <form>
-    <div class="form-group">
-      <label>Image URL:</label>
-      <input type="text" id="image-url"/>
-    </div>
-  </form>`,
-  buttons: {
-    yes: {
-      icon: '<i class="fas fa-check"></i>',
-      label: "Share",
-      callback: (html) => {
-        const imageUrl = html[0].querySelector("#image-url").value;
-        if (!imageUrl) {
-          return ui.notifications.info("You did not provide a valid image.");
-        }
-        const ip = new ImagePopout(imageUrl);
-        ip.render(true);
-        ip.shareImage();
-      }
+  content: "<input type='text' placeholder='Paste link here...' autofocus>",
+  rejectClose: false,
+  callback: (html) => {
+    const imageUrl = html[0].querySelector("input").value;
+    if (!imageUrl) {
+      return ui.notifications.info("You did not provide a valid image.");
     }
+    const ip = new ImagePopout(imageUrl);
+    ip.render(true);
+    ip.shareImage();
   }
-}).render(true);
+});
