7d25cb5b1fe42d45f5e3b44d92f3fe4f522fa691
Zhell <50169243+krbz999@users.noreply.github.com>
2022-11-26 17:00:53 +0100

    Update sneak_attack.js

--- classes/rogue/sneak_attack.js
+++ classes/rogue/sneak_attack.js
@@ -1,23 +1,17 @@
 // Sneak Attack
 // Required modules: itemacro
 
-// applies an effect that increases weapon damage equal to sneak attack.
-// click again to remove the effect.
-
-// if effect, delete it
-const effect = actor.effects.find(i => i.label === "Sneak Attack");
-if ( effect ) return effect.delete();
-
-// if no effect, create it
+const identifier = "sneak-attack";
+const effect = actor.effects.find(e => {
+  return e.getFlag("core", "statusId") === identifier);
+});
+if (effect) return effect.delete();
 const mode = CONST.ACTIVE_EFFECT_MODES.ADD;
-const value = `+${item.system.damage.parts[0][0]}`;
-
-await actor.createEmbeddedDocuments("ActiveEffect", [{
-  changes: [
-    { key: "system.bonuses.mwak.damage", mode, value },
-    { key: "system.bonuses.rwak.damage", mode, value }
-  ],
-  icon: item.img,
-  label: "Sneak Attack",
-  "flags.core.statusId": "sneak-attack"
+const value = `+${actor.getRollData().scale.rogue[identifier]}`;
+const changes = ["mwak", "rwak"].map(wak => {
+  return {key: `system.bonuses.${wak}.damage`, mode, value};
+});
+return actor.createEmbeddedDocuments("ActiveEffect", [{
+  changes, icon: item.img, label: "Sneak Attack",
+  "flags.core.statusId": identifier
 }]);
