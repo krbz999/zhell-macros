08867b09597d85835e5f16ce5e24f49632079556
Zhell <50169243+krbz999@users.noreply.github.com>
2022-11-26 16:55:19 +0100

    Update eloquence.js

--- classes/bard/eloquence.js
+++ classes/bard/eloquence.js
@@ -1,30 +1,25 @@
-/* 
-  Applies a floor of 10 to Persuasion and Deception.
-  Requires having a token selected, or having an assigned character.
-*/
-new Dialog({
+// Eloquence
+// required modules: none
+
+Dialog.prompt({
   title: "Eloquence",
   content: `
   <form>
     <div class="form-group">
       <label>Select Skill</label>
       <div class="form-fields">
-        <select id="eloquence">
+        <select>
           <option value="per">Persuasion</option>
           <option value="dec">Deception</option>
         </select>
       </div>
     </div>
   </form>`,
-  buttons: {
-    go: {
-      icon: "<i class='fa-solid fa-check'></i>",
-      label: "Roll",
-      callback: async (html) => {
-        const skill = html[0].querySelector("#eloquence").value;
-        const a = token?.actor ?? game.user.character;
-        await a.rollSkill(skill, { reliableTalent: true, event });
-      }
-    }
+  rejectClose: false,
+  label: "Roll",
+  callback: async (html) => {
+    const skill = html[0].querySelector("select").value;
+    const a = token?.actor ?? game.user.character;
+    return a.rollSkill(skill, { reliableTalent: true, event });
   }
-}).render(true);
+});
