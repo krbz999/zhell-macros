8b29577efec55ebef5b20a840ed8a86ced6a1322
Zhell <50169243+krbz999@users.noreply.github.com>
2022-11-26 17:12:04 +0100

    Update toll_the_dead.js

--- dnd5e_spells/toll_the_dead.js
+++ dnd5e_spells/toll_the_dead.js
@@ -5,20 +5,7 @@ const target = game.user.targets.first();
 if (!target) return ui.notifications.warn("Please target a token.");
 
 // get consts.
-const { level } = actor.system.details;
-const { value, max } = target.actor.system.attributes.hp;
-
-// calculate formula.
-const dice = level >= 17 ? 4 : level >= 11 ? 3 : level >= 5 ? 2 : 1;
-const size = value < max ? "d12" : "d8";
-
-// determine flavor text.
-const flavor = value < max ? "Toll the Dead - Damage Roll (hurt)" : "Toll the Dead - Damage Roll (full)";
-
-// roll it.
-const use = await item.use();
-if (!use) return;
-await new Roll(`${dice}${size}[necrotic]`).toMessage({
-  flavor,
-  speaker: ChatMessage.getSpeaker({ actor })
-});
+const { value, max, tempmax=0 } = target.actor.system.attributes.hp;
+const formula = value < (max + tempmax) ? "1d12" : "1d8";
+const clone = item.clone({"system.damage.parts": [[formula, "necrotic"]]}, {keepId: true});
+const use = await clone.use({}, {"flags.dnd5e.itemData": clone.toObject()});
