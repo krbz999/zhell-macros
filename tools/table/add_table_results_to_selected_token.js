6f998697e1d358b0bb9d8f11ab93b6eb4f09db8b
Zhell <50169243+krbz999@users.noreply.github.com>
2022-12-01 19:06:37 +0100

    v10 update

--- tools/table/add_table_results_to_selected_token.js
+++ tools/table/add_table_results_to_selected_token.js
@@ -1,22 +1,15 @@
 // roll on a table that contains items and add the item to a selected token's actor.
 
-// use this if the table is in the sidebar.
-const table = game.tables.getName("name of the table");
-
-// use this if the table is in a compendium.
-const tableKey = "key of the compendium that has the table";
-const tableName = "name of the table";
-const pack = game.packs.get(tableKey);
-const tableId = pack.index.find(i => i.name === tableName)._id;
-const table = await pack.getDocument(tableId);
+// get the uuid by right-clicking the book icon in the table's header.
+const table = await fromUuid("uuid of table goes here");
 
 // draw items from compendium or sidebar.
 const receiver = token?.actor;
 if(!table || !receiver) return ui.notifications.warn("Missing table or selected token.");
 const {results} = await table.draw();
 const itemData = (await Promise.all(results.map(i => {
-  const key = i.data.collection;
-  const itemId = i.data.resultId;
+  const key = i.documentCollection;
+  const itemId = i.documentId;
   const uuid = `Compendium.${key}.${itemId}`;
   return key === "Item" ? game.items.get(itemId) : fromUuid(uuid);
 }))).map(i => i.toObject());
