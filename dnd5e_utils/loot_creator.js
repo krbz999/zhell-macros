cd943d17aa1b39553caaa02038b8657f19064a81
Zhell <50169243+krbz999@users.noreply.github.com>
2023-01-13 22:36:11 +0100

    Update loot_creator.js

--- dnd5e_utils/loot_creator.js
+++ dnd5e_utils/loot_creator.js
@@ -10,12 +10,12 @@ const {files} = await FilePicker.browse("public", "icons/commodities/treasure");
 const img = files[Math.floor(Math.random() * files.length)];
 
 // set up input fields.
-const nameField = `<input id="name" type="text" value="Curiosity">`;
-const priceField = `<input id="price" type="number" value="25">`;
-const weightField = `<input id="weight" type="number" value="1">`;
-const quantityField = `<input id="quantity" type="number" value="1">`;
-const descriptionField = `<textarea style="resize: none" id="description" placeholder="Enter text..."></textarea>`;
-const sidebarCheck = `<input type="checkbox" id="sidebarbox">`;
+const nameField = `<input name="name" type="text" value="Curiosity">`;
+const priceField = `<input name="price" type="number" value="25">`;
+const weightField = `<input name="weight" type="number" data-dtype="Number" value="1">`;
+const quantityField = `<input name="quantity" type="number" data-dtype="Number" value="1">`;
+const descriptionField = `<textarea style="resize: none" name="desc" placeholder="Enter text..."></textarea>`;
+const sidebarCheck = `<input name="sidebar" type="checkbox">`;
 
 // set up html.
 const content = `
@@ -58,23 +58,19 @@ new Dialog({
       label: "Create Loot!",
       callback: async (html) => {
         // construct item data.
-        const name = html[0].querySelector("#name").value;
-        const value = html[0].querySelector("#description").value;
-        const price = Number(html[0].querySelector("#price").value);
-        const quantity = Number(html[0].querySelector("#quantity").value);
-        const weight = Number(html[0].querySelector("#weight").value);
-        const itemData = [{
-          name,
+        const form = html[0].querySelector("form");
+        const itemData = {
+          name: form.name.value,
           img,
           type: "loot",
           system: {
-            description: { value },
-            price,
-            quantity,
+            description: {value: form.desc.value},
+            price: {value: form.price.value},
+            quantity: form.quantity.value,
             rarity: "common",
-            weight
+            weight: form.weight.value
           }
-        }];
+        };
         // pick the target or location.
         const crosshairs = await warpgate.crosshairs.show({
           label: "Select recipient or location",
@@ -83,21 +79,19 @@ new Dialog({
         });
         const tokenDocs = !crosshairs.cancelled ? warpgate.crosshairs.collect(crosshairs) : [];
         // pop it in the sidebar
-        const sidebar = html[0].querySelector("#sidebarbox").checked;
-        if (sidebar) await Item.createDocuments(itemData);
+        const sidebar = form.sidebar.checked;
+        if (sidebar) await Item.createDocuments([itemData]);
         
         // if no token was targeted, add the item to a new item pile, initially hidden.
         if (!tokenDocs.length) {
           const updates = {
-            embedded: {
-              Item: { [name]: itemData[0] }
-            },
-            token: { hidden: true }
+            embedded: {Item: {[form.name]: itemData}},
+            token: {hidden: true}
           }
           return warpgate.spawnAt(crosshairs, "Default Item Pile", updates);
         }
         // if a token was targeted, add to their inventory instead.
-        await tokenDocs[0].actor.createEmbeddedDocuments("Item", itemData);
+        await tokenDocs[0].actor.createEmbeddedDocuments("Item", [itemData]);
         // if single item pile it's probably named as that item, so fix that
         const isItemPile = tokenDocs[0].getFlag("item-piles", "data.enabled");
         if (!!isItemPile) await tokenDocs[0].update({ name: "Pile of Loot" });
