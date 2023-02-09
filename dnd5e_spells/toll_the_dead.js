2e8b964d122f4a0424be397421206409ed9a4a30
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-09 19:41:28 +0100

    Update toll_the_dead.js

--- dnd5e_spells/toll_the_dead.js
+++ dnd5e_spells/toll_the_dead.js
@@ -1,11 +1,10 @@
 // Toll the Dead
 // required modules: itemacro.
 
-const target = game.user.targets.first();
-if (!target) return ui.notifications.warn("Please target a token.");
+if (game.user.targets.size !== 1) return ui.notifications.warn("Please target exactly one token.");
 
-// get consts.
-const { value, max, tempmax=0 } = target.actor.system.attributes.hp;
-const formula = value < (max + tempmax) ? "1d12" : "1d8";
+const hp = game.user.targets.first().actor.system.attributes.hp;
+const formula = (hp.value ?? 0) < (hp.max + (hp.tempmax ?? 0)) ? "1d12" : "1d8";
 const clone = item.clone({"system.damage.parts": [[formula, "necrotic"]]}, {keepId: true});
+clone.prepareFinalAttributes();
 const use = await clone.use({}, {"flags.dnd5e.itemData": clone.toObject()});
