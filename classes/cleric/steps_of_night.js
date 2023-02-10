baccf43ad84ae3f699edbabd99e117c34bf6bbee
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-10 13:45:07 +0100

    Update steps_of_night.js

--- classes/cleric/steps_of_night.js
+++ classes/cleric/steps_of_night.js
@@ -1,25 +1,24 @@
 // STEPS OF NIGHT
 // required modules: itemacro
 
-const effect = actor.effects.find(effect => {
-  return effect.getFlag("core", "statusId") === "steps-of-night";
-});
+const id = item.name.slugify({strict: true});
+const effect = actor.effects.find(e => e.flags.core?.statusId === id);
 if (effect) return effect.delete();
 const use = await item.use();
 if (!use) return;
 
 return actor.createEmbeddedDocuments("ActiveEffect", [{
-  label: "Steps of Night",
+  label: item.name,
   changes: [{
     key: "system.attributes.movement.fly",
     mode: CONST.ACTIVE_EFFECT_MODES.UPGRADE,
     value: actor.system.attributes.movement.walk
   }],
-  duration: { seconds: 60 },
+  duration: {seconds: 60},
   icon: item.img,
-  "flags.core.statusId": "steps-of-night",
+  "flags.core.statusId": id,
   "flags.visual-active-effects.data": {
-    intro: "You have a flying speed equal to your walking speed.",
+    intro: "<p>You have a flying speed equal to your walking speed.</p>",
     content: item.system.description.value
   }
 }]);
