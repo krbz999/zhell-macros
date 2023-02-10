c95160ee5fd80178761f9838747fb3f77fab45ae
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-10 13:42:55 +0100

    Update eyes_of_night.js

--- classes/cleric/eyes_of_night.js
+++ classes/cleric/eyes_of_night.js
@@ -2,24 +2,27 @@
 // Required modules: warpgate, itemacro
 
 const range = 120;
-
+const targets = game.user.targets;
 const mod = Math.max(actor.system.abilities.wis.mod, 1);
-const size = game.user.targets.size;
-if (mod < size || size < 1) {
-  ui.notifications.error("Please target an appropriate number of creatures.");
+
+if (!targets.size.between(1, mod)) {
+  ui.notifications.error(`Please target between 1 and ${mod} creatures.`);
   return;
 }
+
 const updates = {
-  actor: { "system.attributes.senses.darkvision": range },
-  token: { sight: { visionMode: "darkvision", range } }
-}
+  actor: {"system.attributes.senses.darkvision": range},
+  token: {"sight.visionMode": "darkvision", "sight.range": range}
+};
+
 const options = {
   name: `Darkvision (${range}ft)`,
   description: `You are being granted ${range} feet of darkvision.`
-}
+};
 
 const use = await item.use();
 if (!use) return;
-[...game.user.targets].map(target => {
-  warpgate.mutate(target.document, updates, {}, options);
-});
+
+for(const target of targets) {
+  await warpgate.mutate(target.document, updates, {}, options);
+}
