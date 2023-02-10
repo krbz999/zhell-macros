a3eeb27281ae22aca80418a2186ea0e269321d6a
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-10 14:12:32 +0100

    Update lay_on_hands.js

--- classes/paladin/lay_on_hands.js
+++ classes/paladin/lay_on_hands.js
@@ -1,52 +1,40 @@
 // LAY ON HANDS
 // required modules: itemacro
 
-const { value } = item.system.uses;
-if ( value < 1 ) {
-    ui.notifications.warn(`${item.name} has no uses left.`);
-    return;
-}
+const uses = item.system.uses;
+if(!uses.value) return ui.notifications.warn(`${item.name} has no uses left.`);
+
 const content = `
-<p>Lay on Hands has ${value} uses left.</p>
+<p>Lay on Hands has ${uses.value} uses left.</p>
 <form>
-    <div class="form-group">
-        <label for="num">Hit points to restore:</label>
-        <div class="form-fields">
-            <input id="num" type="number" value="1"></input>
-        </div>
+  <div class="form-group">
+    <label for="num">Hit points to restore:</label>
+    <div class="form-fields">
+      <input type="number" value="1" autofocus>
     </div>
+  </div>
 </form>`;
 
 const buttons = {
-    heal: {
-        icon: "<i class='fa-solid fa-hand-holding-heart'></i>",
-        label: "Heal!",
-        callback: async (html) => {
-            const number = Number(html[0].querySelector("#num").value);
-            if ( number < 1 || number > value ) {
-                ui.notifications.warn("Invalid number.");
-                return;
-            }
-            await new Roll(`${number}`).toMessage({
-                speaker: ChatMessage.getSpeaker({ actor }),
-                flavor: item.name
-            });
-            return item.update({ "system.uses.value": value - number });
-        }
+  heal: {
+    icon: "<i class='fa-solid fa-hand-holding-heart'></i>",
+    label: "Heal!",
+    callback: async (html) => {
+      const number = Number(html[0].querySelector("#num").value);
+      if(!number.between(1, uses.value)) return ui.notifications.warn("Invalid number.");
+      await new Roll(`${number}`).toMessage({speaker, flavor: item.name});
+      return item.update({"system.uses.value": uses.value - number});
     }
-}
-if ( value >= 5 ) {
-    buttons.cure = {
-        icon: "<i class='fa-solid fa-virus'></i>",
-        label: "Cure!",
-        callback: async (html) => {
-            await ChatMessage.create({
-                content: `${actor.name} cures a disease or poison.`,
-                speaker: ChatMessage.getSpeaker({ actor })
-            });
-            return item.update({ "system.uses.value": value - 5 });
-        }
+  }
+};
+if(uses.value >= 5){
+  buttons.cure = {
+    icon: "<i class='fa-solid fa-virus'></i>",
+    label: "Cure!",
+    callback: async (html) => {
+      await ChatMessage.create({content: `${actor.name} cures a disease or poison.`, speaker});
+      return item.update({"system.uses.value": uses.value - 5});
     }
+  };
 }
-
-new Dialog({ title: "Lay on Hands", content, buttons }).render(true);
+return new Dialog({title: item.name, content, buttons}).render(true);
