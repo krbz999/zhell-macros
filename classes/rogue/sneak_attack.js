6f240c3a059d7080971963188a9bae51557732ce
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-10 14:26:10 +0100

    Update sneak_attack.js

--- classes/rogue/sneak_attack.js
+++ classes/rogue/sneak_attack.js
@@ -1,17 +1,19 @@
 // Sneak Attack
 // Required modules: itemacro
 
-const identifier = "sneak-attack";
-const effect = actor.effects.find(e => {
-  return e.getFlag("core", "statusId") === identifier);
-});
-if (effect) return effect.delete();
+const id = "sneak-attack";
+const effect = actor.effects.find(e => e.flags.core?.statusId === id);
+if(effect) return effect.delete();
+
 const mode = CONST.ACTIVE_EFFECT_MODES.ADD;
-const value = `+${actor.getRollData().scale.rogue[identifier]}`;
+const value = `@scale.rogue.${id}`;
 const changes = ["mwak", "rwak"].map(wak => {
   return {key: `system.bonuses.${wak}.damage`, mode, value};
 });
+
 return actor.createEmbeddedDocuments("ActiveEffect", [{
-  changes, icon: item.img, label: "Sneak Attack",
-  "flags.core.statusId": identifier
+  changes,
+  icon: item.img,
+  label: item.name,
+  "flags.core.statusId": id
 }]);
