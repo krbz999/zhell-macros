4a57d91cf0e6137544e8722321f050cbf4b5c7ed
Zhell <50169243+krbz999@users.noreply.github.com>
2023-02-10 14:20:35 +0100

    Update divine_smite.js

--- classes/paladin/divine_smite.js
+++ classes/paladin/divine_smite.js
@@ -1,60 +1,58 @@
 // DIVINE SMITE
 // required modules: none.
 
-const rollData = foundry.utils.duplicate(actor.getRollData());
-const inputs = Object.entries(rollData.spells).filter(s => {
-  return s[1].value > 0;
-}).map(([key, { value, max }]) => {
-  const crd = key === "pact" ? "Pact Slot" : nth(Number(key.at(-1)));
-  return [key, crd, value, max];
+const data = actor.getRollData();
+const inputs = Object.entries(data.spells).filter(([key, values]) => {
+  return values.value > 0;
+}).map(([key, values]) => {
+  const crd = key === "pact" ? "Pact Slot" : CONFIG.DND5E[key.at(-1)];
+  return [key, crd, values];
 });
-if ( inputs.length < 1 ) {
-  ui.notifications.warn("You have no spell slots remaining.");
-  return;
-}
+if(!inputs.length) return ui.notifications.warn("You have no spell slots remaining.");
 
-const options = inputs.reduce((acc, [key, crd, value, max]) => {
-  return acc + `<option value="${key}">${crd} (${value}/${max})</option>`;
+const options = inputs.reduce((acc, [key, crd, values]) => {
+  return acc + `<option value="${key}">${crd} (${values.value}/${values.max})</option>`;
 }, "");
 
 const content = `
-<form>
+<form class="dnd5e">
   <div class="form-group">
-    <label style="flex: 1;">Spell Slot:</label>
+    <label>Spell Slot:</label>
     <div class="form-fields">
-      <select id="smite-slot">${options}</select>
-      <input type="checkbox" id="extra-die"></input>
-      <label for="extra-die" style="white-space: nowrap;">Extra die</label>
+      <select>${options}</select>
+      <input type="checkbox" id="smite-extra-die">
+      <label for="smite-extra-die" style="white-space: nowrap;">Extra die</label>
     </div>
   </div>
 </form>`;
 
-new Dialog({
-  title: "Divine Smite",
+return new Dialog({
+  title: item.name,
   content,
   buttons: {
     smite: {
       icon: "<i class='fa-solid fa-gavel'></i>",
       label: "Smite!",
-      callback: async (html) => await rollDamage(html)
+      callback: rollDamage
     }
   }
 }).render(true);
 
-function nth(n){return n + (["st","nd","rd"][((n+90)%100-10)%10-1]||"th")}
-
 async function rollDamage(html){
-  const slot = html[0].querySelector("#smite-slot").value;
-  const extra = html[0].querySelector("#extra-die").checked;
-  const level = slot === "pact" ? rollData.spells["pact"].level : Number(slot.at(-1));
+  const slot = html[0].querySelector("select").value;
+  const extra = html[0].querySelector("#smite-extra-die").checked;
+  const level = slot === "pact" ? data.spells["pact"].level : Number(slot.at(-1));
   const dice = Math.min(5, 1 + level) + (extra ? 1 : 0);
   const formula = `${dice}d8`;
-  const damageRoll = await new Item.implementation({
-    type: "feat",
-    name: "Divine Smite",
-    system: { damage: { parts: [[formula, "radiant"]] } }
-  }, { parent: actor }).rollDamage();
-  if ( !damageRoll ) return;
-  const value = rollData.spells[slot].value - 1;
+  const feature = await new Item.implementation({
+    type: "feat", name: item.name,
+    system: {
+      damage: {parts: [[formula, "radiant"]]},
+      actionType: "util"
+    }
+  }, {parent: actor});
+  const roll = await feature.rollDamage({event});
+  if(!roll) return;
+  const value = data.spells[slot].value - 1;
   return actor.update({ [`system.spells.${slot}.value`]: value });
 }
