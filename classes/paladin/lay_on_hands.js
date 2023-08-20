293146528e39e166855420163d7f9614bc093ad7
Zhell <50169243+krbz999@users.noreply.github.com>
2023-08-20 15:46:47 +0200

    Update lay_on_hands.js

--- classes/paladin/lay_on_hands.js
+++ classes/paladin/lay_on_hands.js
@@ -8,9 +8,9 @@ const content = `
 <p>Lay on Hands has ${uses.value} uses left.</p>
 <form class="dnd5e">
   <div class="form-group">
-    <label for="num">Hit points to restore:</label>
+    <label>Hit points to restore:</label>
     <div class="form-fields">
-      <input type="number" value="1" autofocus>
+      <input type="number" value="1" autofocus min="1" max="${uses.value}">
     </div>
   </div>
 </form>`;
@@ -20,21 +20,20 @@ const buttons = {
     icon: "<i class='fa-solid fa-hand-holding-heart'></i>",
     label: "Heal!",
     callback: async (html) => {
-      const number = Number(html[0].querySelector("#num").value);
+      const number = Number(html[0].querySelector("input").value);
       if (!number.between(1, uses.value)) return ui.notifications.warn("Invalid number.");
       await new Roll(`${number}`).toMessage({speaker, flavor: item.name});
       return item.update({"system.uses.value": uses.value - number});
     }
-  }
-};
-if (uses.value >= 5) {
-  buttons.cure = {
+  },
+  cure: {
+    condition: uses.value >= 5,
     icon: "<i class='fa-solid fa-virus'></i>",
     label: "Cure!",
     callback: async (html) => {
       await ChatMessage.create({content: `${actor.name} cures a disease or poison.`, speaker});
       return item.update({"system.uses.value": uses.value - 5});
     }
-  };
-}
+  }
+};
 return new Dialog({title: item.name, content, buttons}).render(true);
