7e8ee964e678e8fe1ba47014f8d75d7c5e0973f4
Zhell <zhellqol@gmail.com>
2023-11-12 17:02:43 +0100

    update bladesong

--- classes/wizard/bladesong.js
+++ classes/wizard/bladesong.js
@@ -2,29 +2,30 @@
 // required modules: itemacro.
 // added benefit with concentrationnotifier and visual-active-effects.
 
-const effect = actor.effects.find(e => e.statuses.has("bladesong"));
+const status = "bladesong";
+const effect = actor.effects.find(e => e.statuses.has(status));
 if (effect) return effect.delete();
-
-const mode = CONST.ACTIVE_EFFECT_MODES.ADD;
-const value = actor.system.abilities.int.mod;
-const changes = [
-  {key: "system.attributes.ac.bonus", mode, value},
-  {key: "system.attributes.movement.walk", mode, value: 10},
-  {key: "flags.dnd5e.concentrationBonus", mode, value: `+${value}`}
-];
-if (actor.classes.wizard.system.levels >= 14) {
-  changes.push({key: "system.bonuses.mwak.damage", mode, value: `+${value}`});
-}
 
 const use = await item.use();
 if (!use) return;
 
 return actor.createEmbeddedDocuments("ActiveEffect", [{
-  changes,
+  changes: [
+    {key: "system.attributes.ac.bonus"},
+    {key: "system.attributes.movement.walk", value: 10},
+    {key: "flags.dnd5e.concentrationBonus"},
+    actor.classes.wizard.system.levels >= 14 ? {key: "system.bonuses.mwak.damage"} : null
+  ].reduce((acc, obj) => {
+    if (obj) acc.push(foundry.utils.mergeObject(obj, {
+      mode: CONST.ACTIVE_EFFECT_MODES.ADD,
+      value: "+@abilities.int.mod"
+    }, {overwrite: false}));
+    return acc;
+  }, []),
   icon: item.img,
   name: item.name,
   duration: {seconds: 60},
-  statuses: ["bladesong"],
-  description: "<p>You are under the effects of Bladesong.</p>",
-  "flags.visual-active-effects.data.content": `<p>... text goes here ...</p>`
+  statuses: [status],
+  description: `<p>You are under the effects of ${item.name}.</p>`,
+  "flags.visual-active-effects.data.content": item.system.description.value
 }]);
