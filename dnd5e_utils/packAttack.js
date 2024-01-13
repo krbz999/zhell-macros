81712cc57906f1cc665429a7cc389e4364c4ca75
Zhell <zhellqol@gmail.com>
2024-01-13 15:07:08 +0100

    Update pack attack tool with custom app

--- dnd5e_utils/packAttack.js
+++ dnd5e_utils/packAttack.js
@@ -1,60 +1,219 @@
-// PACK ATTACKS
-// select a group of tokens, then select what item to attack with.
+/**
+ * A tool for managing hordes or big groups of tokens and their attacks and damage rolls.
+ */
 
-const pack = canvas.tokens.controlled.map(i => i.actor);
-const actorIds = pack.map(i => i.id);
-if (!new Set(actorIds).size) {
-  ui.notifications.warn("No selected tokens!");
-  return;
-}
-
-const itemsWithAttacks = actorIds.map(id => {
-  return game.actors.get(id);
-}).reduce((acc, {items}) => {
-  acc.push(...items);
-  return acc;
-}, []).filter(item => item.hasAttack).map(item => item.name);
-const names = new Set(itemsWithAttacks);
-
-const options = names.reduce((acc, name) => {
-  return acc + `<option value="${name}">${name}</option>`;
-}, "");
-const content = `
-<p>Choose pack attack:</p>
-<form>
-  <div class="form-group">
-    <div class="form-fields">
-      <select id="attack">${options}</select>
-    </div>
-  </div>
-</form>`;
-
-return Dialog.prompt({
-  title: "Pack Attack!",
-  content,
-  rejectClose: false,
-  callback: async (html, event) => {
-    const ev = event;
-    let leader;
-    const name = html[0].querySelector("#attack").value;
-    let content = "";
-
-    for (const member of pack) {
-      const item = await member.items.getName(name);
-      if (!item) continue;
-      leader = member;
-      const attack = await item.rollAttack({event: ev, chatMessage: false});
-      if (!attack) continue;
-      game.dice3d?.showForRoll(attack, game.user, true);
-      if (attack.isCritical) content += `<p>Attack Roll: ${attack.total} (Critical!)</p>`;
-      else if (attack.isFumble) content += `<p>Attack Roll: ${attack.total} (Fumbled)</p>`;
-      else content += `<p>Attack Roll: ${attack.total}</p>`;
-    }
-    const item = leader.items.getName(name);
-    await item.displayCard();
-    await ChatMessage.create({content, "flags.core.canPopout": true});
-    if (item.hasDamage) for (const member of pack) {
-      await member.items.getName(name)?.rollDamage();
-    }
+class PackAttack extends Dialog {
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    options.classes.push("pack-attack");
+    options.height = "auto";
+    return options;
   }
-});
+
+  static async create() {
+    return new this({
+      title: "Pack Attack Tool",
+      buttons: {close: {icon: "<i class='fa-solid fa-check'></i>", label: "Close"}}
+    }).render(true);
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+    html[0].querySelectorAll("[data-action]").forEach(n => {
+      switch (n.dataset.action) {
+        case "item":
+          n.addEventListener("change", this._onItem.bind(this)); break;
+        case "attack":
+          n.addEventListener("click", this._onAttack.bind(this)); break;
+        case "damage":
+          n.addEventListener("click", this._onDamage.bind(this)); break;
+        case "ping":
+          n.addEventListener("click", this._onPing.bind(this)); break;
+      }
+    });
+  }
+
+  /**
+   * Ping or pull to a token.
+   * @param {Event} event     Initiating click event.
+   */
+  async _onPing(event) {
+    const tokenId = event.currentTarget.closest("[data-token-id]").dataset.tokenId;
+    const origin = this.ids[tokenId].token.object.center;
+    canvas.ping(origin, {});
+  }
+
+  /**
+   * Roll damage for a token actor's item.
+   * @param {Event} event     Initiating click event.
+   */
+  async _onDamage(event) {
+    const tokenId = event.currentTarget.closest("[data-token-id]").dataset.tokenId;
+    const itemId = event.currentTarget.closest("[data-token-id]").querySelector("[data-action=item]").value;
+    const item = this.ids[tokenId].token.actor.items.get(itemId);
+    if (!item) return;
+    const dmg = await item.rollDamage({event: event});
+    if (!dmg) return;
+    foundry.utils.setProperty(this, `ids.${tokenId}.damage`, dmg.total);
+    this.render();
+  }
+
+  /**
+   * Roll attack for a token actor's item.
+   * @param {Event} event     Initiating click event.
+   */
+  async _onAttack(event) {
+    const tokenId = event.currentTarget.closest("[data-token-id]").dataset.tokenId;
+    const itemId = event.currentTarget.closest("[data-token-id]").querySelector("[data-action=item]").value;
+    const item = this.ids[tokenId].token.actor.items.get(itemId);
+    if (!item) return;
+    const atk = await item.rollAttack({event: event});
+    if (!atk) return;
+    foundry.utils.setProperty(this, `ids.${tokenId}.attack`, atk.total);
+    this.render();
+  }
+
+  /**
+   * Unset attack and damage when changing the item, then re-render.
+   * @param {Event} event     Initiating change event.
+   */
+  _onItem(event) {
+    const value = event.currentTarget.value;
+    const tokenId = event.currentTarget.closest("[data-token-id]").dataset.tokenId;
+    foundry.utils.setProperty(this, `ids.${tokenId}.selected`, value);
+    foundry.utils.setProperty(this, `ids.${tokenId}.attack`, null);
+    foundry.utils.setProperty(this, `ids.${tokenId}.damage`, null);
+    this.render();
+  }
+
+  /**
+   * Get token documents you are owner of and whose actor have an item with an attack and damage roll.
+   * @type {Set<TokenDocument5e>}
+   */
+  get tokens() {
+    const tokens = new Set();
+    const actors = new Set();
+    for (const token of canvas.scene.tokens) {
+      if (!token.actor) continue;
+      const isOwner = token.actor.isOwner;
+      const isPlayer = game.user.isGM && game.users.some(user => {
+        user.active && token.actor.testUserPermission(user, "OWNER");
+      });
+      if (!isOwner || isPlayer) continue;
+      const hasItem = token.actor.items.some(item => item.hasAttack && item.hasDamage);
+      if (hasItem && !actors.has(token.actor)) {
+        actors.add(token.actor);
+        tokens.add(token);
+      }
+    }
+    return tokens;
+  }
+
+  /**
+   * Get select options for the items with an attack and damage roll from a token.
+   * @param {TokenDocument5e} token     The token document.
+   * @returns {string}                  Select options with item id and item name.
+   */
+  itemSelectOptions(token) {
+    const choices = token.actor.items.reduce((acc, item) => {
+      const valid = item.hasAttack && item.hasDamage;
+      if (valid) acc[item.id] = item.name;
+      return acc;
+    }, {});
+    const selected = this.ids?.[token.id]?.selected || null;
+    const options = {hash: {selected: selected, blank: "-"}};
+    return HandlebarsHelpers.selectOptions(choices, options);
+  }
+
+  /**
+   * Disable the select for this token?
+   * @param {TokenDocument5e} token     The token document.
+   * @returns {boolean}
+   */
+  disableSelect(token) {
+    return this.getAttackRoll(token) !== null;
+  }
+
+  /**
+   * Get the attack roll's value for this token.
+   * @param {TokenDocument5e} token     The token document.
+   * @returns {number|null}             The rolled value or null if not rolled.
+   */
+  getAttackRoll(token) {
+    const atk = this.ids?.[token.id]?.attack;
+    return parseInt(atk) || null;
+  }
+
+  /**
+   * Get the damage roll's value for this token.
+   * @param {TokenDocument5e} token     The token document.
+   * @returns {number|null}             The rolled value or null if not rolled.
+   */
+  getDamageRoll(token) {
+    const atk = this.ids?.[token.id]?.damage;
+    return parseInt(atk) || null;
+  }
+
+  /**
+   * Retrieve the current data model in use.
+   * @type {PackAttackModel}
+   */
+  get tokenData() {
+    const oldids = this.ids || {};
+    const ids = {};
+    for (const token of this.tokens) {
+      const options = this.itemSelectOptions(token);
+      ids[token.id] = {};
+      if (token.id in oldids) {
+        ids[token.id] = {...oldids[token.id]};
+      }
+      ids[token.id].options = options;
+      ids[token.id].token = token;
+      ids[token.id].disableSelect = this.disableSelect(token);
+      ids[token.id].attack = this.getAttackRoll(token);
+      ids[token.id].damage = this.getDamageRoll(token);
+    }
+    return this.ids = ids;
+  }
+
+  /** @override */
+  render(force = false, options = {}) {
+    this.data.content = this.getContent();
+    return super.render(force, options);
+  }
+
+  /**
+   * Construct the 'template' of this dialog.
+   * @returns {string}      The content property.
+   */
+  getContent() {
+    const style = `
+    <style>
+    .pack-attack .form-fields a[data-action] {
+      flex: 0 0 40px;
+      text-align: center;
+      border: 1px #c1b7b7 groove;
+      border-radius: 5px;
+      background-color: rgba(0, 0, 0, 0.15);
+      font-family: 'Bruno Ace';
+    }
+    </style>`;
+
+    let content = `<form class="dnd5e">`;
+    for (const {options, token, disableSelect, attack, damage} of Object.values(this.tokenData)) {
+      content += `
+      <div class="form-group" data-token-id="${token.id}">
+        <label><a data-action="ping">${token.name}</a></label>
+        <div class="form-fields">
+          <select data-action="item" ${disableSelect ? "disabled" : ""}>${options}</select>
+          <a data-action="attack">${attack ?? "<i class='fa-solid fa-dice-d20'></i>"}</a>
+          <a data-action="damage">${damage ?? "<i class='fa-solid fa-dice'></i>"}</a>
+        </div>
+      </div>`;
+    }
+    content += "</form>";
+    return style + content;
+  }
+}
+PackAttack.create();
