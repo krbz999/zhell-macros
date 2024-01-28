daaeaae1ad50b331aed1e97cffb9219bdc13a62e
Zhell <zhellqol@gmail.com>
2024-01-28 12:54:40 +0100

    Update temphp macro, init message hook

--- dnd5e_utils/grant_temphp_to_selected.js
+++ dnd5e_utils/grant_temphp_to_selected.js
@@ -1,36 +1,30 @@
 /**
  * Click to set temporary hit points of selected tokens.
- * Shift-click to remove temporary hit points of selected tokens.
+ * Shift-click to remove all temporary hit points of selected tokens.
  */
 
-if (event.shiftKey) {
-  for (const token of canvas.tokens.controlled) {
-    token.actor?.update({"system.attributes.hp.temp": null});
-  }
-  return;
-}
+const actors = Array.from(canvas.tokens.controlled.reduce((acc, token) => {
+  if (token.actor) acc.add(token.actor);
+  return acc;
+}, new Set()));
 
-new Dialog({
+if (event.shiftKey) return Promise.all(actors.map(actor => actor.update({"system.attributes.hp.temp": null})));
+
+return Dialog.prompt({
   title: "Add temporary hit points",
   content: `
   <form class="dnd5e">
     <div class="form-group">
       <label>Temp HP</label>
       <div class="form-fields">
-        <input type="number" data-dtype="Number" name="temphp" autofocus>
+        <input type="number" name="temphp" autofocus>
       </div>
     </div>
   </form>`,
-  buttons: {
-    go: {
-      icon: "<i class='fa-solid fa-check'></i>",
-      label: "Apply",
-      callback: async (html) => {
-        const update = new FormDataExtended(html[0].querySelector("form")).object;
-        for (const token of canvas.tokens.controlled) {
-          await token.actor?.applyTempHP(update.temphp);
-        }
-      }
-    }
+  label: "Apply",
+  callback: async (html) => {
+    const {temphp} = new FormDataExtended(html[0].querySelector("FORM")).object;
+    if (!temphp) return null;
+    return Promise.all(actors.map(actor => actor.applyTempHP(temphp)));
   }
-}).render(true);
+});
--- dnd5e_utils/hook_remove_initiative_rolls.js
+++ dnd5e_utils/hook_remove_initiative_rolls.js
@@ -1,9 +1,7 @@
 // delete all initiative rolls from chat. Only show 3d dice rolls for player characters.
-Hooks.on("preCreateChatMessage", (msg, roll, context, userId) => {
-  if (!msg.getFlag("core", "initiativeRoll")) return;
-  context.temporary = true;
-  const actor = game.actors.get(roll.speaker.actor);
-  if (!!actor && actor.hasPlayerOwner) {
-    game.dice3d?.showForRoll(msg.roll, game.user, true);
-  }
+Hooks.on("preCreateChatMessage", (message) => {
+  if (!message.flags.core?.initiativeRoll) return;
+  const actor = game.actors.get(message.speaker.actor);
+  if (actor?.hasPlayerOwner) game.dice3d?.showForRoll(message.rolls[0], game.user, true);
+  return false;
 });
