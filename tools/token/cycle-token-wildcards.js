309a6b28a502ef927a0fdb52db4402f9ee334976
Zhell <50169243+krbz999@users.noreply.github.com>
2024-03-05 01:21:10 +0100

    Fix reverse of token wildcard cycling

--- tools/token/cycle-token-wildcards.js
+++ tools/token/cycle-token-wildcards.js
@@ -13,5 +13,5 @@ const wildcardImages = await token.document.baseActor.getTokenImages();
 const currentIndex = wildcardImages.indexOf(token.document.texture.src);
 let next;
 if (!isReverse) next = (currentIndex + 1 === wildcardImages.length) ? 0 : (currentIndex + 1);
-else next = (currentIndex - 1 <= 0) ? wildcardImages.at(-1) : (currentIndex - 1);
+else next = (currentIndex > 0) ? (currentIndex - 1) : wildcardImages.length - 1;
 await token.document.update({"texture.src": wildcardImages[next]});
