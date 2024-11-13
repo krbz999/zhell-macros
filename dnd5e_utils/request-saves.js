2453de324d12f8cca860afaa4d0cf1d33e514483
Zhell <50169243+krbz999@users.noreply.github.com>
2024-11-13 01:13:07 +0100

    Create request-saves.js

---
+++ dnd5e_utils/request-saves.js
@@ -0,0 +1,35 @@
+// Request a saving throw (possibly concentration).
+
+const ability = new foundry.data.fields.StringField({
+  required: true, choices: CONFIG.DND5E.abilities, label: "Ability"
+}).toFormGroup({}, {name: "ability"}).outerHTML;
+const dc = new foundry.data.fields.NumberField({
+  min: 0, max: 30, integer: true, nullable: false, label: "DC"
+}).toFormGroup({}, {name: "dc", value: 10}).outerHTML;
+const isConc = new foundry.data.fields.BooleanField({
+  label: "Concentration", hint: "Is this a save for concentration?"
+}).toFormGroup({}, {name: "concentration"}).outerHTML;
+
+const dataset = await foundry.applications.api.DialogV2.prompt({
+  content: `<fieldset>${[ability, dc, isConc].join("")}</fieldset>`,
+  rejectClose: false,
+  modal: true,
+  window: {title: "Request Saving Throw"},
+  position: {width: 400, height: "auto"},
+  ok: {callback: (event, button) => new FormDataExtended(button.form).object}
+});
+if (!dataset) return;
+
+dataset.type = dataset.concentration ? "concentration" : "save";
+
+const chatData = {
+  user: game.user.id,
+  content: await renderTemplate("systems/dnd5e/templates/chat/request-card.hbs", {
+    buttonLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true}),
+    hiddenLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true, hideDC: true}),
+    dataset: {...dataset, action: "rollRequest", visibility: "all"}
+  }),
+  flavor: game.i18n.localize("EDITOR.DND5E.Inline.RollRequest"),
+  speaker: ChatMessage.implementation.getSpeaker({user: game.user})
+};
+await ChatMessage.implementation.create(chatData);
