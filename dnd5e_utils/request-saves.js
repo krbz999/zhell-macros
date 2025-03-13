d54d00bda930d8df4d8746483f6d16319f2ecaf5
Zhell <50169243+krbz999@users.noreply.github.com>
2025-03-14 00:34:13 +0100

    Update request-saves.js

--- dnd5e_utils/request-saves.js
+++ dnd5e_utils/request-saves.js
@@ -10,28 +10,29 @@ const isConc = new foundry.data.fields.BooleanField({
   label: "Concentration", hint: "Is this a save for concentration?"
 }).toFormGroup({}, {name: "concentration"}).outerHTML;
 
-const dataset = await foundry.applications.api.DialogV2.prompt({
+foundry.applications.api.DialogV2.prompt({
   content: `<fieldset>${[ability, dc, isConc].join("")}</fieldset>`,
   rejectClose: false,
   modal: true,
   window: {title: "Request Saving Throw"},
   position: {width: 400, height: "auto"},
-  ok: {callback: (event, button) => new FormDataExtended(button.form).object}
+  ok: {callback: callback},
 });
-if (!dataset) return;
 
-dataset.type = dataset.concentration ? "concentration" : "save";
-
-const chatData = {
-  user: game.user.id,
-  content: await renderTemplate("systems/dnd5e/templates/chat/request-card.hbs", {
-    buttons: [{
-      buttonLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true}),
-      hiddenLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true, hideDC: true}),
-      dataset: {...dataset, action: "rollRequest", visibility: "all"},
-    }],
-  }),
-  flavor: game.i18n.localize("EDITOR.DND5E.Inline.RollRequest"),
-  speaker: ChatMessage.implementation.getSpeaker({user: game.user})
-};
-await ChatMessage.implementation.create(chatData);
+async function callback(event, button) {
+  const dataset = new FormDataExtended(button.form).object;
+  dataset.type = dataset.concentration ? "concentration" : "save";
+  const chatData = {
+    user: game.user.id,
+    content: await renderTemplate("systems/dnd5e/templates/chat/request-card.hbs", {
+      buttons: [{
+        buttonLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true}),
+        hiddenLabel: dnd5e.enrichers.createRollLabel({...dataset, format: "short", icon: true, hideDC: true}),
+        dataset: {...dataset, action: "rollRequest", visibility: "all"},
+      }],
+    }),
+    flavor: game.i18n.localize("EDITOR.DND5E.Inline.RollRequest"),
+    speaker: ChatMessage.implementation.getSpeaker({user: game.user})
+  };
+  await ChatMessage.implementation.create(chatData);
+}
