7786d94fe92525e15a2c0718e8db7e734b713762
Zhell <50169243+krbz999@users.noreply.github.com>
2025-07-27 13:51:57 +0200

    Update move-defeated-combatants.js

--- dnd5e_utils/move-defeated-combatants.js
+++ dnd5e_utils/move-defeated-combatants.js
@@ -1,26 +1,19 @@
-// Display a kill count message?
-const displayMessage = true;
+// Move the defeated combatants to the top left of the scene?
+const moveDefeated = true;
 
-/* -------------------------------------------------- */
+// Display a kill count chat message?
+const displayMessage = false;
 
-const combat = game.combat;
 const defeated = game.combat.combatants.filter(c => c.isDefeated);
-
-// delete defeated.
 const deleteIds = defeated.map(c => c.id);
-await combat.deleteEmbeddedDocuments("Combatant", deleteIds);
-
-const {x, y} = canvas.scene.dimensions.sceneRect;
-const updates = defeated.map(c => ({_id: c.token.id, x, y}));
-await canvas.scene.updateEmbeddedDocuments("Token", updates, {animation: {movementSpeed: 12}});
-
+await game.combat.deleteEmbeddedDocuments("Combatant", deleteIds);
+if (moveDefeated) {
+ const { x, y } = canvas.scene.dimensions.sceneRect;
+ const updates = defeated.map(c => ({ _id: c.token.id, x, y }));
+ await canvas.scene.updateEmbeddedDocuments("Token", updates, { animation: { movementSpeed: 12 } });
+}
 if (displayMessage) {
-  const enemies = canvas.scene.tokens.filter(token => {
-    return token.disposition === CONST.TOKEN_DISPOSITIONS.HOSTILE;
-  });
-  const kills = enemies.filter(token => {
-    const hp = token.actor?.system.attributes?.hp;
-    return hp && !hp.value;
-  }).length;
-  ChatMessage.implementation.create({content: `Kills: ${kills} / ${enemies.length}`});
+ const enemies = canvas.scene.tokens.filter(token => token.disposition === CONST.TOKEN_DISPOSITIONS.HOSTILE);
+ const kills = enemies.filter(token => !token.actor.system.attributes.hp.value).length;
+ ChatMessage.implementation.create({ content: `<p>Kills: ${kills} / ${enemies.length}</p>`});
 }
